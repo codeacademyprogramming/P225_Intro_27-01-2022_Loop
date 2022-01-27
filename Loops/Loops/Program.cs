30137425fabe17967570d1ff345276bb7298d14c
HamidMammadov <hamidvm@code.edu.az>
2022-01-27 18:15:15 +0400

    First Commit

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 0;
+
+            //if(a < 0)
+            //{
+            //    Console.Read();
+            //}
+
+            //if(a > 5)
+            //{
+            //    Console.Read();
+            //}
+
+            //if(a == 5)
+            //{
+            //    Console.WriteLine("Carpe diem");
+            //}
+
+            //a++;
+
+            //if (a == 5)
+            //{
+            //    Console.WriteLine("Carpe diem");
+            //}
+
+            //a++;
+
+            //if (a == 5)
+            //{
+            //    Console.WriteLine("Carpe diem");
+            //}
+
+            //a++;
+
+            //if (a == 5)
+            //{
+            //    Console.WriteLine("Carpe diem");
+            //}
+
+            //a++;
+
+            //if (a == 5)
+            //{
+            //    Console.WriteLine("Carpe diem");
+            //}
+
+            //a++;
+
+            //int a = 4;
+
+            //if (a < 0)
+            //{
+            //    Console.Read();
+            //}
+
+            //if (a > 5)
+            //{
+            //    Console.Read();
+            //}
+
+            //while (a <= 5)
+            //{
+            //    if (a == 5)
+            //    {
+            //        Console.WriteLine("Carpe diem");
+            //        break;
+            //    }
+            //    a++;
+            //}
+
+            //int a = 4;
+
+            //while (a > 0 && a <= 5)
+            //{
+            //    if (a == 5)
+            //    {
+            //        Console.WriteLine("Carpe diem");
+            //        break;
+            //    }
+            //    a++;
+            //}
+
+            //for (int a = 0; a <= 5 ; a++)
+            //{
+            //    if (a == 5)
+            //    {
+            //        Console.WriteLine("Carpe diem");
+            //    }
+            //}
+
+            //for (int a = 0; a > 0 && a <= 5; a++)
+            //{
+            //    if (a == 5)
+            //    {
+            //        Console.WriteLine("Carpe diem");
+            //    }
+            //}
+
+            //int a = 7;
+
+            //do
+            //{
+            //    Console.WriteLine("Carpe diem");
+
+            //    a++;
+            //} while (a <= 5);
+
+            //do
+            //{
+            //    Console.WriteLine("Carpe diem");
+
+            //    a++;
+            //} while (a > 0 && a <= 5);
+
+
+            //int result = 0;
+            //int i = 0;
+
+            //while (i <= 50)
+            //{
+            //    if(i % 2 == 0)
+            //    {
+            //        result += i; //result = result + i;
+            //    }
+
+            //    i++;
+            //}
+
+            //Console.WriteLine(result);
+
+            //int result = 0;
+
+            //for (int i = 0; i <= 50; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        result += i; //result = result + i;
+            //    }
+            //}
+
+            //Console.WriteLine(result);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; i < 5; j++)
+                {
+                  
+                }
+            }
+
+
+            if (true)
+            {
+                if (true)
+                {
+                    for (int i = 0; i < 50; i++)
+                    {
+                        if (true)
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                if (true)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
